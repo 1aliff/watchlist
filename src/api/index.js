ace47c3533f4be9c78242cc94f4333469bcd3014
1aliff <xy.aliff@gmail.com>
2021-02-02 19:31:31 +0800

    Cleaner

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,13 @@
 const getData = async (initialQuery) => {
   const api_key = process.env.REACT_APP_API_KEY
-
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${initialQuery}&language=en-US&page=1&include_adult=false`
 
   try {
-    const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${initialQuery}&language=en-US&page=1&include_adult=false`)
+    const res = await fetch(url)
     const data = await res.json()
     return data
   } catch (err) {
-    alert(`Something went wrong ${err}`)
+    alert(`Something went wrong 😧 ${err}`)
   }
 }
 
